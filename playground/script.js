bc7ce48463b9fb732cee8285b8ae5108cf820686
Jeff Rodgers <jeffrey.rodgers@outlook.com>
2025-02-15 12:38:12 -0500

    0.1.0 Final Commit

---
+++ playground/script.js
@@ -0,0 +1,7 @@
+import "/dist/kepler-ui.js";
+
+const testButton = document.querySelector("#TestButton");
+
+testButton.addEventListener("click", () => {
+    console.log("Test Button Clicked");
+});
--- public/script.js
+++
@@ -1,8 +0,0 @@
-import "/dist/kepler-ui.js";
-
-const button2 = document.querySelector("#getValueButton");
-
-button2.addEventListener("click", () => {
-    const menu = document.querySelector("#multiMenu");
-    console.log("Menu Value", menu.value);
-});
